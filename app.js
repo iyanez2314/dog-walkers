83655f8e5fbb0bfaa1d21d81c326e5b26ad724a5
Carl Contreras <contrerascarl09@gmail.com>
2022-02-09 21:45:13 -0600

    server not connecting

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlbars')
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 
 
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./src/**/*.{main.js}"],
     theme: {
       extend: {},
     },
