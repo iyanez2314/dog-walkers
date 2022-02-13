72c47eb2691bcee8cf6161277a601c9d5970ec7b
Isaac yanez <isaac231467@gmail.com>
2022-02-12 20:58:21 -0600

    added files for the home routes

--- contollers/index.js
+++ contollers/index.js
@@ -2,7 +2,10 @@ const router = require('express').Router()
 
 const apiRoutes = require('./api');
 const dashboardRoutes = require('./dashboard');
+const homeRoutes = require('./home-routes');
 
+
+router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api/', apiRoutes);
 
