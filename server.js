ece138be447d433fbcd050ef41056e3733bfe792
Carl Contreras <contrerascarl09@gmail.com>
2022-02-13 19:14:56 -0600

    able to get data from back end but not everthing

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const express = require('express')
-const sequelize = require('./config/connection');
+const express = require("express");
+const sequelize = require("./config/connection");
+const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3001
-
-
+const PORT = process.env.PORT || 8001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
+app.use(require("./contollers"));
 
-app.use(require('./contollers'));
+app.get("/", function (req, res) {
+  res.json("dog Walkers");
+});
 
-// sync sequelize models to db 
+app.get("/results", () => {});
+
+// sync sequelize models to db
 sequelize.sync().then(() => {
-    app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
