a42fd370753bcd2fc60694f4ae9f05af73a84763
Tanner Brieger <tanner29@gmail.com>
2022-02-16 07:14:16 -0600

    uncommented /login if so if logged in it takes you back to dashboard without relogging

--- contollers/home-routes.js
+++ contollers/home-routes.js
@@ -8,10 +8,10 @@ router.get('/', (req, res) => {
 
 //
 router.get('/login', (req, res) => {
-//    if (req.session.loggedIn) {
-//      res.redirect('/');
-//      return;
-//    }
+    if (req.session.loggedIn) {
+      res.redirect('/dashboard');
+      return;
+    }
  
     res.render('login');
   });
